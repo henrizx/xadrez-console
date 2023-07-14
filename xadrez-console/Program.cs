2d4d8bf2bd5e8218c8a45617ce84925b7b5bc374
henrizx <marcelohenrizx@gmail.com>
2023-07-14 15:05:32 -0300

    Projeto criado, junto com a criacao de classe posicao

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using tabuleiro;
+
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            posicao p;
+
+            p = new posicao(3, 4);
+
+            Console.WriteLine($"posicao: {p}");
+        }
+    }
+}

37cc6d1a6a4b4982aa3808a5275a002cd26db66a
henrizx <marcelohenrizx@gmail.com>
2023-07-24 15:58:51 -0300

    adicionando posicao no tabulerio

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,21 +7,9 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new posicao(0, 2));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new posicao(2, 9));
-
-                Tela.imprimeTabuleiro(tab);
-            }
-            catch (TabulerioException e)
-            {
-                Console.WriteLine(e.Message);
-
-                Console.ReadLine();
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
         }
     }
 }

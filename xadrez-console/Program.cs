bcbf5f6b9576b6c235eec11ed8cadb78a733e3b0
henrizx <marcelohenrizx@gmail.com>
2023-07-25 16:18:53 -0300

    imprimindo metodo de cor tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,9 +7,22 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new posicao(0, 2));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new posicao(3, 7));
+                Tela.imprimeTabuleiro(tab);
+            }
+            catch(TabulerioException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            Console.ReadLine();
         }
     }
 }

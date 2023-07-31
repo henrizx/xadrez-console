acce3038b5ae1dd1aa8994ce04530c39fa990066
henrizx <marcelohenrizx@gmail.com>
2023-07-31 17:24:25 -0300

    validando excepetions movimento, cor, turno etc

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -9,33 +9,45 @@ namespace xadrez_console
         {
             try
             {
-               PartidaDeXadrez partida = new PartidaDeXadrez();
-                while (!partida.terminada) 
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                while (!partida.terminada)
                 {
-                    Console.Clear();
-                    Tela.imprimeTabuleiro(partida.tab);
-                    Console.WriteLine();
-                    Console.Write("Digite a posição de origem: ");
-                    posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    try
+                    {
+                        Console.Clear();
+                        Tela.imprimeTabuleiro(partida.tab);
+                        Console.WriteLine();
+                        Console.WriteLine($"Turno: {partida.turno}");
+                        Console.WriteLine($"Aguardando jogada {partida.jogadorAtual}");
+
+                        Console.WriteLine();
+                        Console.Write("Digite a posição de origem: ");
+                        posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                        Console.Clear();
+                        Tela.imprimeTabuleiro(partida.tab, posicoesPossiveis);
 
 
-                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+                        Console.Write("Destino: ");
+                        posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-                    Console.Clear();
-                    Tela.imprimeTabuleiro(partida.tab, posicoesPossiveis);
-
-
-                    Console.Write("Destino: ");
-                    posicao destino = Tela.lerPosicaoXadrez().toPosicao();
-
-                    partida.executaMoviment(origem, destino);
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabulerioException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
                 }
 
                 Tela.imprimeTabuleiro(partida.tab);
 
-              
+
             }
-            catch(TabulerioException e)
+            catch (TabulerioException e)
             {
                 Console.WriteLine(e.Message);
 
--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -1,13 +1,11 @@
-﻿
-using tabuleiro;
-
+﻿using tabuleiro;
 namespace xadrez
 {
     class PartidaDeXadrez
     {
         public Tabuleiro tab { get; private set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual;
         public bool terminada { get; private set; }
 
 
@@ -16,8 +14,9 @@ namespace xadrez
             tab = new Tabuleiro(8, 8);
             turno = 1;
             jogadorAtual = Cor.Branca;
-            colocarPecas();
             terminada = false;
+            colocarPecas();
+
 
         }
         public void executaMoviment(posicao origim, posicao destino)
@@ -26,6 +25,39 @@ namespace xadrez
             p.incrementatQtMovimentos();
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
+        }
+
+        public void realizaJogada(posicao origem, posicao destino)
+        {
+            executaMoviment(origem, destino);
+            turno++;
+            mudaJogador();
+        }
+        public void validarPosicaoDeOrigem(posicao pos)
+        {
+            if (tab.peca(pos) == null)
+            {
+                throw new TabulerioException("não existe peça na posição escolhida!");
+            }
+            if (jogadorAtual != tab.peca(pos).cor)
+            {
+                throw new TabulerioException("A peça de origem escolhida não é sua!");
+            }
+            if (!tab.peca(pos).existeMovimentosPossiveis())
+            {
+                throw new TabulerioException("Não há movimentos possiveis para sua peça!");
+            }
+        }
+        private void mudaJogador()
+        {
+            if (jogadorAtual == Cor.Branca)
+            {
+                jogadorAtual = Cor.Preta;
+            }
+            else
+            {
+                jogadorAtual = Cor.Branca;
+            }
         }
         private void colocarPecas()
         {
@@ -38,7 +70,7 @@ namespace xadrez
 
 
 
-           tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('c', 7).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('c', 7).toPosicao());
             tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('c', 8).toPosicao());
             tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('d', 7).toPosicao());
             tab.colocarPeca(new Torre(tab, Cor.Preta), new PosicaoXadrez('e', 7).toPosicao());
